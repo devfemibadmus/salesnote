f412b355e6c2aeea57f69a3e3d31490082eb2354
Dev Femi Badmus <devfemibadmus@gmail.com>
2026-02-01 16:07:44 +0000

    mobile: define application route constants

---
+++ mobile/lib/app/routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const auth = '/auth';
+  static const onboarding = '/onboarding';
+  static const home = '/home';
+  static const sales = '/sales';
+  static const items = '/items';
+  static const newSale = '/sales/new';
+  static const shop = '/shop';
+  static const notification = '/notification';
+}
