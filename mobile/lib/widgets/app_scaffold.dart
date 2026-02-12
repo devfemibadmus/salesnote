d1c4944dfd8f2eb35f19f84496d4936751570ca0
Dev Femi Badmus <devfemibadmus@gmail.com>
2026-02-12 20:17:04 +0000

    mobile: add reusable widget for app scaffold

---
+++ mobile/lib/widgets/app_scaffold.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({
+    super.key,
+    required this.title,
+    required this.body,
+  });
+
+  final String title;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: body,
+        ),
+      ),
+    );
+  }
+}
